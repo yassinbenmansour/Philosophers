e0e0fccfd3b33ba312bc88e9663b812fb16d5712
yabenman <benmansour.yassinne@gmail.com>
2025-02-26 03:16:14 +0000

    update

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: yabenman <yabenman@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 20:25:41 by yabenman          #+#    #+#             */
-/*   Updated: 2025/02/23 20:38:01 by yabenman         ###   ########.fr       */
+/*   Updated: 2025/02/26 00:55:43 by yabenman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <pthread.h>
+
+
+typedef struct s_philo
+{
+	int				ph_id;
+	int				ph_t_mourir;
+	int				ph_t_manger;
+	int				ph_t_dort;
+    int				ph_status;
+
+	
+}	t_philo;
+
 
 int		ph_error(char *s);
 
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,9 +6,13 @@
 /*   By: yabenman <yabenman@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 20:26:24 by yabenman          #+#    #+#             */
-/*   Updated: 2025/02/23 20:45:07 by yabenman         ###   ########.fr       */
+/*   Updated: 2025/02/25 02:02:19 by yabenman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+// ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
+
 
 #include "../inc/philosophers.h"
 
